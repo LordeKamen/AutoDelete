349dae50f92bdf1d15cc504dcc307bec965e4075
Murilo Calegari de Souza <murilo.calegari.souza@gmail.com>
2018-10-14 21:45:48 -0300

    Just playing around

--- AutoDelete.ps1
+++ AutoDelete.ps1
@@ -6,7 +6,7 @@ $timeObject = new-timespan -Days $timeDays
 $files = Get-ChildItem $PathToDelete -Recurse -File
 
 
-# Delete files that are older than the time specified in $timeDays
+# Delete files that are older than the time specified in $timeDays.
 foreach ($file in $files) {
     $file_age = (Get-Item $file.FullName).CreationTime
     if (((Get-Date) - $file_age) -gt $timeObject) {
@@ -15,7 +15,7 @@ foreach ($file in $files) {
     }
 }
 
-# Delete empty directories
+# Delete empty directories.
 do {
   $dirs = Get-ChildItem $PathToDelete -directory -recurse | Where-Object { (Get-ChildItem $_.fullName).count -eq 0 } | Select-Object -expandproperty FullName
   $dirs | Foreach-Object { Remove-Item $_ -Recurse -Force }
